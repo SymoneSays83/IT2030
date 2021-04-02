101613a495d97f86c484c488fdc70ec397946f54
Nina Payne <S01198568@acad.tri-c.edu>
2021-04-02 15:43:03 -0400

    Project 2 commit

---
+++ Project 2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_2.Models;
+
+namespace Project_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DA = 0;
+            ViewBag.T = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculateModel model)
+        {
+            if(ModelState.IsValid) {
+                decimal? discount = model.DiscountAmount();
+                ViewBag.DA = discount;
+                ViewBag.T = model.Subtotal - discount;
+            }
+            else
+            {
+                ViewBag.DA = 0;
+                ViewBag.T = 0;
+            }
+            return View(model);
+        }
+    }
+}
