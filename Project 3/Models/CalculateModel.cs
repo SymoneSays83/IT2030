ac8ea9cfb705bdc94c2755a020d2d111926ff982
Nina Payne <S01198568@acad.tri-c.edu>
2021-04-25 21:34:23 -0400

    Project 3 commit

---
+++ Project 3/Models/CalculateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_3.Models
+{
+    public class CalculateModel
+    {
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Please enter a subtotal greater than zero.")]
+        public decimal? Subtotal { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage ="Please enter a percent between 0 and 100.")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? DiscountAmount()
+        {
+            decimal? discount = Subtotal * (DiscountPercent / 100);
+            return discount;
+        }
+    }
+}
