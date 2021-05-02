87acbd16b95870cee7292661fcc65776cf517aa4
Nina Payne <S01198568@acad.tri-c.edu>
2021-05-02 17:39:24 -0400

    Assignment 15 Commit

---
+++ Assignment 15/Ch15Ex1ClassSchedule/ClassSchedule/Components/DayFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClassSchedule.Models;
+
+namespace ClassSchedule.Components
+{
+    public class DayFilter : ViewComponent
+    {
+        private IRepository<Day> days { get; set; }
+        public DayFilter(IRepository<Day> rep) => days = rep;
+
+        public IViewComponentResult Invoke()
+        {
+            var daysData = days.List(new QueryOptions<Day>
+            {
+                OrderBy = d => d.DayId
+            });
+            return View(daysData);
+        }
+    }
+}
---
+++ Assignment 15/Ch15Ex1ClassSchedule/ClassSchedule/TagHelpers/MyLinkButtonTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+
+namespace ClassSchedule.TagHelpers
+{
+    public class MyLinkButtonTagHelper : TagHelper
+    {
+        private LinkGenerator linkGenerator;
+        public MyLinkButtonTagHelper(LinkGenerator linkGen) => linkGenerator = linkGen;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewCtx { get; set; }
+
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string action = Action ?? ViewCtx.RouteData.Values["action"].ToString();
+            string controller = Controller ?? ViewCtx.RouteData.Values["controller"].ToString();
+            var segment = (string.IsNullOrEmpty(Id)) ? null : new { Id };
+
+            string url = linkGenerator.GetPathByAction(action, controller, segment);
+            string css = (IsActive) ? "btn btn-dark" : "btn btn-outline-dark";
+
+            output.BuildLink(url, css);
+        }
+    }
+}
